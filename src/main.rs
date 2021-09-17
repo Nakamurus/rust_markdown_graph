5011a488f94ab67535fd183083b356ec5042d7f5
nakamurus <3872mn@gmail.com>
2021-09-17 22:15:40 +0900

    reads input markdown file and does stdout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{App, Arg};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
     let matches = App::new("markdown_outliner")
@@ -13,7 +16,21 @@ fn main() {
         )
         .get_matches();
 
-    if let Some(i) = matches.value_of("INPUT") {
-        println!("Value for input: {}", i);
+    let file_name:&str = matches.value_of("INPUT").expect("Please specify a markdown file argument");
+
+    if let Ok(lines) = read_lines(file_name) {
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
+        }
+    } else {
+        panic!("File does not exists in the specified path");
     }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
